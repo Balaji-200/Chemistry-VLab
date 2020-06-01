c1bd20c5a8a2f3789730bf53bf1c2fb187328f8c
Balaji Durai <baladurai420@gmail.com>
2020-06-01 17:49:09 +0530

    Added Sample Animation.

---
+++ js/animation.js
@@ -0,0 +1,26 @@
+const scene = new THREE.Scene()
+const canvas = document.getElementById('canvas')
+const aspectRatio = canvas.offsetWidth/canvas.offsetHeight
+const camera = new THREE.PerspectiveCamera(75,aspectRatio,0.1,1000)
+const renderer = new THREE.WebGLRenderer({antialias:true})
+renderer.setSize(canvas.offsetWidth,canvas.offsetHeight);
+const geometry = new THREE.BoxGeometry(3,2,2)
+// const material = new THREE.MeshBasicMaterial({ color:0xff0000 })
+const texture = new THREE.TextureLoader().load('../assets/crate.jpg');
+const material = new THREE.MeshBasicMaterial({map:texture})
+const cube = new THREE.Mesh(geometry,material)
+scene.add(cube);
+camera.position.z=5;
+canvas.appendChild(renderer.domElement);
+window.addEventListener('resize',()=>{
+    renderer.setSize(canvas.offsetWidth,canvas.offsetHeight);
+    camera.aspect(canvas.offsetWidth/canvas.offsetHeight);
+    camera.updateProjectMatrix();
+})
+function animate(){
+    requestAnimationFrame(animate);
+    cube.rotation.x+=0.01;
+    cube.rotation.y+=0.01;
+    renderer.render(scene,camera);
+}
+animate();
