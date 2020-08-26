ab77fd78fb071538dc5e7ff2dc93b22fc6b4520b
Balaji Durai <baladurai420@gmail.com>
2020-08-26 21:10:57 +0530

    Added 4 levels in zinc experiment

---
+++ Experiments/Amount-of-zinc-in-brass/index.js
@@ -0,0 +1,55 @@
+const game = document.getElementById("game");
+// const level01 = document.getElementById('level-01');
+var levelNo = 1;
+function Next() {
+  switch (levelNo) {
+    case 1:
+      if (checklevel01()) {
+        level02();
+        nextLevel();
+      }
+      break;
+    case 2:
+      if(checklevel02()){
+        level03();
+        nextLevel();
+      }
+      break;
+    case 3:
+      if(checklevel03()){
+        nextLevel();
+        level04();
+      }
+      break;
+    case 4:
+      if(checklevel04()){
+        nextLevel()
+      }
+      break;
+  }
+}
+
+function nextLevel() {
+  const level = document.getElementById(`level-0${levelNo}`);
+  const nextLevel = document.getElementById(`level-0${levelNo + 1}`);
+  if (level.classList.contains("d-show")) {
+    level.classList.remove("d-show");
+    level.classList.add("d-none");
+  } else {
+    level.classList.remove("d-none");
+    level.classList.add("d-show");
+  }
+  if (nextLevel != null) {
+    if (nextLevel.classList.contains("d-show")) {
+      nextLevel.classList.remove("d-show");
+      nextLevel.classList.add("d-none");
+    } else {
+      nextLevel.classList.remove("d-none");
+      nextLevel.classList.add("d-show");
+    }
+  }
+  levelNo < 4? levelNo++ : (location.href = "./experiment.html");
+  location.href = `#${nextLevel.id}`;
+  location.href = `#top`;
+}
+level01();
---
+++ Experiments/Amount-of-zinc-in-brass/script.js
@@ -0,0 +1,188 @@
+//################################# GLOBAL DECLARATIONS ###################################################//
+function clickListener(options) {
+  var clicks = 0;
+  for (let i = 0; i < options.children.length-1; i++) {
+    options.children[i].addEventListener("click", () => {
+      if (clicks == 0) options.children[i].classList.add("option-clicked");
+      clicks++;
+    });
+  }
+}
+var durationRight = 0.7;
+var durationLeft = 0.7;
+const level04Options = [
+    "It’s silvery-white and light-weight",
+    "Excellent choice for making decorative pieces",
+    "Yellow Gold color",
+    "Used in electronic applications",
+    "Used in Haber’s process as a catalyst",
+    "High corrosion resistance",
+    "Used in radiation protection",
+    "Highly castable"
+  ];
+  const level04WrongOptions = [
+  "It’s silvery-white and light-weight",
+  "Used in Haber’s process as a catalyst",
+  "Used in radiation protection"
+  ]
+//################################## LEVEL-01 ###############################################//
+function level01() {
+  const options = document.getElementById("options-level-01");
+  for (let i = 0; i <= options.children.length - 1; i++) {
+    options.children[i].addEventListener("click", () => {
+      if (options.children[i].classList.contains("wrong")) {
+        options.children[i].classList.add("bg-danger");
+        setTimeout(() => {
+          alert("Wrong Answers!! , Try Again");
+          window.location.href = "./landing-page.html";
+        }, 100);
+      } else {
+        options.children[i].classList.add("bg-success");
+      }
+    });
+  }
+}
+function checklevel01() {
+  var correctAnswers = 0;
+  const options = document.getElementById("options-level-01");
+  for (let i = 0; i <= options.children.length - 1; i++) {
+    if (options.children[i].classList.contains("bg-success")) {
+      correctAnswers++;
+    }
+  }
+  if (correctAnswers == 7) {
+    return true;
+  } else {
+    alert("Choose all the correct answers, Try Again");
+    window.location.href = "./landing-page.html";
+  }
+}
+//#################################################################################//
+
+function level02() {
+  const options = document.getElementById("options-level-02");
+  clickListener(options);
+}
+function checklevel02() {
+  const options = document.getElementById("options-level-02");
+  for (let i = 0; i <= options.children.length - 1; i++) {
+    if (options.children[i].classList.contains("option-clicked")) {
+      if (options.children[i].children[0].getAttribute("alt") == "blue-pill") {
+        return true;
+      } else {
+        alert("Wrong Answers!! , Try Again");
+        window.location.href = "./landing-page.html";
+      }
+    }
+  }
+}
+//#################################################################################//
+
+function level03() {
+  const options = document.getElementById("options-level-03");
+  for (let i = 0; i <= options.children.length - 1; i++) {
+    options.children[i].addEventListener("click", () => {
+      if (options.children[i].classList.contains("wrong")) {
+        options.children[i].classList.add("bg-danger");
+        setTimeout(() => {
+          alert("Wrong Answers!! , Try Again");
+          window.location.href = "./landing-page.html";
+        }, 100);
+      } else {
+        options.children[i].classList.add("bg-success");
+      }
+    });
+  }
+}
+function checklevel03() {
+  var correctAnswers = 0;
+  const options = document.getElementById("options-level-03");
+  for (let i = 0; i <= options.children.length - 1; i++) {
+    if (options.children[i].classList.contains("bg-success")) {
+      correctAnswers++;
+    }
+  }
+  if (correctAnswers == 4) {
+    return true;
+  } else {
+    alert("Choose all the correct answers, Try Again");
+    window.location.href = "./landing-page.html";
+  }
+}
+//####################################################################################//
+function level04() {
+  const options = document.getElementById("options-level-04");
+  const drop1 = document.getElementById("drop1");
+  const drop4 = document.getElementById("drop4");
+  drop1.style.animationDuration = "0.8s";
+  drop4.style.animationDuration = "0.8s";
+  level04Options.map((option, index) => {
+    options.appendChild(createOption(option, index + 1));
+  });
+}
+
+function createOption(option, id) {
+  const div = document.createElement("div");
+  div.classList.add(
+    "option",
+    "font-weight-light",
+    "m-1",
+    "justify-content-center",
+    "text-center",
+    "p-1",
+    "bg-purple",
+    "align-items-center",
+    "d-flex"
+  );
+  div.innerText = option;
+  div.setAttribute("id", `drag${id}`);
+  div.setAttribute("draggable", "true");
+  div.setAttribute("ondragstart", "drag(event)");
+  if (id % 2 == 0) {
+    div.classList.add("slideRight");
+    div.style.animationDuration = `${(durationLeft += 0.1)}s`;
+  } else {
+    div.classList.add("slideRight");
+    div.style.animationDuration = `${(durationRight += 0.1)}s`;
+  }
+  return div;
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  document.getElementById(
+    ev.target.id
+  ).innerText = document.getElementById(data).innerText;
+}
+
+function checklevel04() {
+  var wrongAnswers = [];
+  for (let i = 1; i <= 5; i++) {
+    const drop = document.getElementById(`drop${i}`);
+    for (let j = 1; j <= 5; j++) {
+      if (j === i) continue;
+      const nextDrop = document.getElementById(`drop${j}`);
+      if (drop.innerText == nextDrop.innerText) {
+        alert("No two Drop boxes can have same options");
+        return false;
+      }
+    }
+    wrongAnswers.push(
+    level04WrongOptions.filter(el=> drop.innerText == el));
+  }
+  if(wrongAnswers.filter(el=> el.length>0).length > 0){
+    alert("Wrong Answers , Try again");
+        window.location.href = './landing-page.html';
+  }else{
+    return true;
+  }
+}
+//####################################################################################//
