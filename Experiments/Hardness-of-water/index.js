002a505035db56e265ac5409ad012855cc86a75f
Balaji Durai <baladurai420@gmail.com>
2020-09-03 20:52:36 +0530

    Added Hard water experiment

---
+++ Experiments/Hardness-of-water/index.js
@@ -0,0 +1,61 @@
+const game = document.getElementById("game");
+// const level01 = document.getElementById('level-01');
+var levelNo = 1;
+function Next() {
+  switch (levelNo) {
+    case 1:
+      if (checklevel01()) {
+        nextLevel();
+        level02();
+      }
+      break;
+    case 2:
+      if (checklevel02()) {
+        nextLevel();
+        level03();
+      }
+      break;
+    case 3:
+      if (checklevel03()) {
+        nextLevel();
+        // level04();
+      }
+      break;
+    case 4:
+      if (checklevel04()) {
+        nextLevel();
+        level05();
+      }
+      break;
+    case 5:
+      if (checklevel05()) {
+        nextLevel();
+      }
+      break;
+  }
+}
+
+function nextLevel() {
+  const level = document.getElementById(`level-0${levelNo}`);
+  const nextLevel = document.getElementById(`level-0${levelNo + 1}`);
+  if (level.classList.contains("d-show")) {
+    level.classList.remove("d-show");
+    level.classList.add("d-none");
+  } else {
+    level.classList.remove("d-none");
+    level.classList.add("d-show");
+  }
+  if (nextLevel != null) {
+    if (nextLevel.classList.contains("d-show")) {
+      nextLevel.classList.remove("d-show");
+      nextLevel.classList.add("d-none");
+    } else {
+      nextLevel.classList.remove("d-none");
+      nextLevel.classList.add("d-show");
+    }
+  }
+  levelNo < 3 ? levelNo++ : (location.href = "./experiment.html");
+  location.href = `#${nextLevel.id}`;
+  location.href = `#top`;
+}
+level01();
---
+++ Experiments/Hardness-of-water/script.js
@@ -0,0 +1,176 @@
+//################################### GLOBAL DECLARATIONS ######################################//
+
+function clickListener(options, CSSclass) {
+  var clicks = 0;
+  for (let i = 0; i < options.children.length; i++) {
+    options.children[i].addEventListener("click", () => {
+      if (clicks == 0) options.children[i].classList.add(CSSclass);
+      clicks++;
+    });
+  }
+}
+
+var durationRight = 0.7;
+var durationLeft = 0.7;
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  document.getElementById(ev.target.id).innerHTML = document.getElementById(
+    data
+  ).innerHTML;
+}
+
+function createOption(option, id, addClass) {
+  const div = document.createElement("div");
+  div.classList.add(
+    `${addClass}`,
+    "font-weight-bold",
+    "m-1",
+    "justify-content-center",
+    "text-center",
+    "p-1",
+    "bg-violet",
+    "align-items-center",
+    "d-flex"
+  );
+  div.innerHTML = option;
+  div.setAttribute("id", `drag${id}`);
+  div.setAttribute("draggable", "true");
+  div.setAttribute("ondragstart", "drag(event)");
+  if (id % 2 == 0) {
+    div.classList.add("slideRight");
+    div.style.animationDuration = `${(durationLeft += 0.1)}s`;
+  } else {
+    div.classList.add("slideRight");
+    div.style.animationDuration = `${(durationRight += 0.1)}s`;
+  }
+  return div;
+}
+
+const level02options = [
+  `Ca<sup>2+</sup>`,
+  `Na<sup>+</sup>`,
+  `OH<sup>-</sup>`,
+  `HCO<sub>3</sub><sup>-</sup>`,
+  `Fe<sup>2+</sup>`,
+  `Cl<sup>-</sup>`,
+  `K<sup>+</sup>`,
+  `Mg<sup>2+</sup>`,
+  `CO<sub>3</sub><sup>2-</sup>`,
+  `NO<sub>3</sub><sup>-</sup>`,
+  `SO<sub>4</sub><sup>2-</sup>`,
+  `Mn<sup>2+</sup>`,
+];
+const level02wrongOptions = [
+  `K<sup>+</sup>`,
+  `Na<sup>+</sup>`,
+  `OH<sup>-</sup>`,
+];
+const level03options = [
+  "Lather does not form easily",
+  "Large amount of sludge and scale formation in hot water boilers",
+  "Not suitable for drinking",
+  "Forms a lot of lather easily",
+  "Not suitable in many industries like sugar industry, paper industry, laundry, pharmaceutical industry, etc.",
+  "Cannot used in concrete making",
+];
+const level03wrongOptions = ["Forms a lot of lather easily"];
+//##############################################################################################//
+function level01() {
+  const options = document.getElementById("level-01-options");
+  clickListener(options, "option-clicked-background");
+}
+function checklevel01() {
+  const options = document.getElementById("level-01-options");
+  for (let i = 0; i < options.children.length; i++) {
+    if (options.children[i].classList.contains("option-clicked-background"))
+      if (options.children[i].innerText == "Origin of Hardness of water") {
+        return true;
+      } else {
+        alert("Wrong Answer, Try Again!!");
+        window.location.href = "./landing-page.html";
+      }
+  }
+}
+//##############################################################################################//
+function level02() {
+  const options = document.getElementById("level-02-options");
+  const drop1 = document.getElementById("drop-level-02-1");
+  const drop6 = document.getElementById("drop-level-02-6");
+  drop1.style.animationDuration = "0.8s";
+  drop6.style.animationDuration = "0.8s";
+  level02options.map((option, index) => {
+    options.appendChild(
+      createOption(option, `-level-02-${index + 1}`, "level-02-option")
+    );
+  });
+}
+function checklevel02() {
+  var wrongAnswers = [];
+  for (let i = 1; i <= 6; i++) {
+    const drop = document.getElementById(`drop-level-02-${i}`);
+    for (let j = 1; j <= 6; j++) {
+      if (j === i) continue;
+      const nextDrop = document.getElementById(`drop-level-02-${j}`);
+      if (drop.innerHTML == nextDrop.innerHTML) {
+        alert("No two Drop boxes can have same options");
+        return false;
+      }
+    }
+    wrongAnswers.push(level02wrongOptions.filter((el) => drop.innerHTML == el));
+  }
+  if (wrongAnswers.filter((el) => el.length > 0).length > 0) {
+    alert("Wrong Answers , Try again");
+    window.location.href = "./landing-page.html";
+  } else {
+    durationRight = 0.7;
+    durationLeft = 0.7;
+    return true;
+  }
+}
+//##############################################################################################//
+function level03() {
+  const options = document.getElementById("level-03-options");
+  const drop1 = document.getElementById("drop-level-03-1");
+  const drop5 = document.getElementById("drop-level-03-5");
+  drop1.style.animationDuration = "0.8s";
+  drop5.style.animationDuration = "0.8s";
+  level03options.map((option, index) => {
+    options.appendChild(
+      createOption(option, `-level-03-${index + 1}`, "level-03-option")
+    );
+  });
+}
+function checklevel03() {
+  var wrongAnswers = [];
+  for (let i = 1; i <= 5; i++) {
+    const drop = document.getElementById(`drop-level-03-${i}`);
+    for (let j = 1; j <= 5; j++) {
+      if (j === i) continue;
+      const nextDrop = document.getElementById(`drop-level-03-${j}`);
+      if (drop.innerHTML == nextDrop.innerHTML) {
+        alert("No two Drop boxes can have same options");
+        return false;
+      }
+    }
+    wrongAnswers.push(level03wrongOptions.filter((el) => drop.innerHTML == el));
+  }
+  if (wrongAnswers.filter((el) => el.length > 0).length > 0) {
+    alert("Wrong Answers , Try again");
+    window.location.href = "./landing-page.html";
+  } else {
+    durationRight = 0.7;
+    durationLeft = 0.7;
+    return true;
+  }
+}
+//##############################################################################################//
